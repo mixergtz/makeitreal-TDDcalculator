6299668923727f8513ef3035e2351134f42eb735
Mixer Gutierrez <mixergutierrez@gmail.com>
2014-11-02 13:45:10 -0500

    Add some DRY

--- spec/lib/calculadora_spec.rb
+++ spec/lib/calculadora_spec.rb
@@ -2,63 +2,46 @@ require "spec_helper"
 require "calculadora"
 
 describe Calculator do
+  let(:calculadora) { Calculator.new }
 
   it "should add two numbers passed in params" do
-    calculadora = Calculator.new
-    suma = calculadora.sumar(10, 20)
-    suma.should == 30
+    calculadora.sumar(10, 20).should == 30
   end
 
   it "should add multiple numbers passed in params" do
-    calculadora = Calculator.new
-    suma = calculadora.sumar(5, 4, 2, 5, 8)
-    suma.should == 24
+    calculadora.sumar(5, 4, 2, 5, 8).should == 24
   end
 
   it "should add multiple numbers passed in params even if negative" do
-    calculadora = Calculator.new
-    suma = calculadora.sumar(5, -4, 2, 5, 8)
-    suma.should == 16
+    calculadora.sumar(5, -4, 2, 5, 8).should == 16
   end
 
   it "should subtract two numbers passed in params" do
-    calculadora = Calculator.new
-    resta = calculadora.restar(5, 4)
-    resta.should == 1
+    calculadora.restar(5, 4).should == 1
   end
 
   it "should subtract two numbers passed in params even if negative" do
-    calculadora = Calculator.new
-    resta = calculadora.restar(5, -4)
-    resta.should == 9
+    calculadora.restar(5, -4).should == 9
   end
 
   it "should subtract multiple numbers passed in params" do
-    calculadora = Calculator.new
-    resta = calculadora.restar(10, 4, 6)
-    resta.should == 0
+    calculadora.restar(10, 4, 6).should == 0
   end
 
   it "should multiply two numbers passed in params" do
-    calculadora = Calculator.new
-    multiplicacion = calculadora.multiplicar(10, 2)
-    multiplicacion.should == 20
+    calculadora.multiplicar(10, 2).should == 20
   end
 
   it "should divide two numbers passed in params" do
-    calculadora = Calculator.new
-    multiplicacion = calculadora.dividir(10, 2)
-    multiplicacion.should == 5
+    calculadora.dividir(10, 2).should == 5
   end
 
   it "should evaluate a string passed in " do
-    resultado = Calculator.evaluar("1 + 4 + 8 - 10 + 2 * 4")
-    resultado.should == 11
+    Calculator.evaluar("1 + 4 + 8 - 10 + 2 * 4").should == 11
   end
 
   it "should evaluate nested expressions in string" do
-    resultado = Calculator.evaluar("10+(2*4)-10/2")
-    resultado.should == 13
+    Calculator.evaluar("10+(2*4)-10/2").should == 13
   end
 
 end
